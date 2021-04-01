90883a44745942758fe9ec979689fb743f2b8b16
Parker Brandt <parker.brandt101@gmail.com>
2021-04-01 15:47:34 +0000

    Completed a more detailed prototype of the main function and added function declarations to be filled out later

--- project1.c
+++ project1.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
 #define BUF_SIZE 16
 
 typedef enum bool {false, true} bool;
@@ -18,6 +22,7 @@ typedef struct Process Process;
 
 // Prototype Functions
 char* readline(int fd);
+
 int countContextSwitches(Process *process);
 double calcCPUUtilization(Process *process);
 double calcThroughput(Process *process);
@@ -78,18 +83,88 @@ int main(int argc, char *argv[])
 		fd = open(filename, O_RDONLY, 0);
 
 
-	// Read the first two lines to 
+	// Read the first two lines to get the initial data
+	char* firstLine;
+	char* secondLine;
+	char* data1, data2;
+
+	strcpy(firstLine, readline(fd));
+	strcpy(firstLine, readline(fd));
+
+	// TEST OUTPUT
+	dprintf(STDERR, "First Line: %s\n", firstLine);
+	dprintf(STDERR, "Second Line: %s\n", secondLine);
+
+	// Parse through the two lines to get the data
+	numProcesses = atoi(firstLine);
+	
+	// Separate the second line at the space to get the two numbers
+	bool isFirst = true;
+	for(int i = 0; i < BUF_SIZE; i++)
+	{
+		if(isFirst == true)
+		{
+			if(secondLine[i] == ' ')
+			{
+				isFirst = false;
+				
+			}
+		}
+		else
+		{
+
+			if(secondLine[i] = '\0')
+				break;
+		}
+	}
 
 	// Read through the file and add information to the Process array
-	processes = (Process*)malloc(numProcesses * sizeof(Process));
-
+	processes = (Process*)malloc(numInstructions * sizeof(Process));
 	
+	char* line;
+	for(int i = 0; i < numInstructions; i++)
+	{
+		// Get the line
+		line = readline(fd);
+
+		// Parse the data
+		
+
+		// Update the data inside the processes array
+		processes[i].pid;
+		processes[i].burst;
+		processes[i].priority;
+	}
 
 	// Calculate information about the processes and output
-	
+	cpuUtil = calcCPUUtilization(processes);
+	throughput = calcThroughput(processes);
+	turnAroundTime = calcTurnaroundTime(processes);
+	waitTime = calcWaitTime(processes);
+	responseTime = calcResponseTime(processes);	
 
 	// Output
 	
+	// Voluntary context switches
+	printf("%d\n", numContextSwitches);
+
+	// Non-voluntary context switches
+	printf("%d\n", numNonvolContextSwitches);
+
+	// CPU Utilization
+	printf("%.2f\n", cpuUtil);
+
+	// Average Throughput
+	printf("%.2f\n", throughput);
+
+	// Average Turnaround Time
+	printf("%.2f\n", turnAroundTime);
+
+	// Average Wait Time
+	printf("%.2f\n", waitTime);
+
+	// Average Response Time
+	printf("%.2f\n", responseTime);
 
 	exit(EXIT_SUCCESS);
 }
@@ -100,6 +175,7 @@ int main(int argc, char *argv[])
 /**
   * Reads a line from the file pointed to by the file descriptor
   * Returns the string obtained from that file
+  * TODO: May have to add a line number parameter
   */
 char* readline(int fd)
 {
@@ -122,4 +198,64 @@ char* readline(int fd)
 	}
 
 	return buffer;
+}
+
+
+/**
+  *
+  */
+double calcCPUUtilization(Process *process)
+{
+	double cpuUtil;
+
+
+
+	return cpuUtil;
+}
+
+
+/**
+  * 
+  */
+double calcThroughput(Process *process)
+{
+	double throughput;
+
+
+	return throughput;
+}
+
+
+/**
+  *
+  */
+double calcTurnaroundTime(Process *process)
+{
+	double turnaroundTime;
+
+
+	return turnaroundTime;
+}
+
+
+/**
+  *
+  */
+double calcWaitTime(Process *process)
+{
+	double waitTime;
+
+	return waitTime;
+}
+
+
+/**
+  *
+  */
+double calcResponseTime(Process *process)
+{
+	double responseTime;
+
+
+	return responseTime;
 }
